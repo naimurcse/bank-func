4eab015d01a61d3177e13e71be04365c9060197a
naimurcse <2015naim@gmail.com>
2022-05-20 12:02:39 +0600

    display-statistics HTML CSS Done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const perSFTprice = 5100;
+const utillityCharge = 200000;
+const registationCharge = 300000;
+const flatSFT = 900;
+
+const flatPrice = (perSFTprice * flatSFT) + utillityCharge;
+console.log(flatPrice);
