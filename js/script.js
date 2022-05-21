95d7d8a1b26ca6b06f50bbb4b30292f1a187e097
naimurcse <2015naim@gmail.com>
2022-05-21 08:14:48 +0600

    Update Balance Total

--- js/script.js
+++ js/script.js
@@ -7,24 +7,46 @@ function getInputValue(id) {
     return inputValue;
 }
 
-// Get and Update Text Value
-function updateValue(id, newAmount) {
-    let amount = document.getElementById(id);
-    amount = amount.innerText;
-    amount = parseFloat(amount);
-    const total = amount + newAmount;
-    document.getElementById(id).innerText = total;
+// Get and Update Total Deposit and Withdraw
+function updateTotal(id, newAmount) {
+    let previousAmount = document.getElementById(id);
+    previousAmount = previousAmount.innerText;
+    previousAmount = parseFloat(previousAmount);
+    const newTotal = previousAmount + newAmount;
+    document.getElementById(id).innerText = newTotal;
+}
+
+// Get and Update Balance Total
+function updateBalance(newAmount, isAdd) {
+    let balanceAmount = document.getElementById("display-balance");
+    balanceAmount = balanceAmount.innerText;
+    balanceAmount = parseFloat(balanceAmount);
+
+    // Update Balance Total
+    let newBalance;
+    if (isAdd == true) {
+        newBalance = balanceAmount + newAmount;
+    } else {
+        newBalance = balanceAmount - newAmount;
+    }
+    document.getElementById("display-balance").innerText = newBalance;
 }
 
 
 // Event Handle by Deposit Btn
 document.getElementById("deposit-btn").addEventListener("click", function() {
     const depositAmount = getInputValue("deposit-input");
-    updateValue("display-deposit", depositAmount);
+    if (depositAmount > 0) {
+        updateTotal("display-deposit", depositAmount);
+        updateBalance(depositAmount, true);
+    }
 });
 
-// Event Handle by Withdrow Btn
-document.getElementById("withdrow-btn").addEventListener("click", function() {
-    const withdrowAmount = getInputValue("withdrow-input");
-    updateValue("display-withdrow", withdrowAmount);
+// Event Handle by withdraw Btn
+document.getElementById("withdraw-btn").addEventListener("click", function() {
+    const withdrawAmount = getInputValue("withdraw-input");
+    if (withdrawAmount > 0) {
+        updateTotal("display-withdraw", withdrawAmount);
+        updateBalance(withdrawAmount, false);
+    }
 });
