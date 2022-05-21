cae2286ef077344cca31b5982feb70a85bf66f7c
naimurcse <2015naim@gmail.com>
2022-05-21 09:29:56 +0600

    Created another update balance function

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 // Get Input Value Function
 function getInputValue(id) {
     let inputValue = document.getElementById(id);
-    inputValue = inputValue.value;
-    inputValue = parseFloat(inputValue);
+    inputValue = parseFloat(inputValue.value);
     document.getElementById(id).value = "";
     return inputValue;
 }
 
 // Get and Update Total Deposit and Withdraw
 function updateTotal(id, newAmount) {
-    let previousAmount = document.getElementById(id);
-    previousAmount = previousAmount.innerText;
-    previousAmount = parseFloat(previousAmount);
-    const newTotal = previousAmount + newAmount;
-    document.getElementById(id).innerText = newTotal;
+    const previousAmount = getInnerTextValue(id);
+    document.getElementById(id).innerText = previousAmount + newAmount;
 }
+
+function getInnerTextValue(id) {
+    let balanceAmount = document.getElementById(id);
+    balanceAmount = parseFloat(balanceAmount.innerText);
+    return balanceAmount;
+}
+
 
 // Get and Update Balance Total
 function updateBalance(newAmount, isAdd) {
-    let balanceAmount = document.getElementById("display-balance");
-    balanceAmount = balanceAmount.innerText;
-    balanceAmount = parseFloat(balanceAmount);
-
-    // Update Balance Total
+    const balanceAmount = getInnerTextValue("display-balance");
+    // Update Balance Total 
     let newBalance;
     if (isAdd == true) {
         newBalance = balanceAmount + newAmount;
@@ -30,6 +30,15 @@ function updateBalance(newAmount, isAdd) {
         newBalance = balanceAmount - newAmount;
     }
     document.getElementById("display-balance").innerText = newBalance;
+    // return balanceAmount;
+}
+
+function updateBalanceSecWay() {
+    const depositTotal = getInnerTextValue("display-deposit");
+    const withdrawTotal = getInnerTextValue("display-withdraw");
+    const initialBalance = 1290;
+    const newBalanceTotal = (depositTotal + initialBalance) - withdrawTotal;
+    document.getElementById("display-balance").innerText = newBalanceTotal;
 }
 
 
@@ -38,15 +47,17 @@ document.getElementById("deposit-btn").addEventListener("click", function() {
     const depositAmount = getInputValue("deposit-input");
     if (depositAmount > 0) {
         updateTotal("display-deposit", depositAmount);
-        updateBalance(depositAmount, true);
+        // updateBalance(depositAmount, true);
+        updateBalanceSecWay();
     }
 });
 
 // Event Handle by withdraw Btn
 document.getElementById("withdraw-btn").addEventListener("click", function() {
     const withdrawAmount = getInputValue("withdraw-input");
-    if (withdrawAmount > 0) {
+    if (withdrawAmount > 0 && withdrawAmount <= getInnerTextValue("display-balance")) {
         updateTotal("display-withdraw", withdrawAmount);
-        updateBalance(withdrawAmount, false);
+        // updateBalance(withdrawAmount, false);
+        updateBalanceSecWay();
     }
 });
